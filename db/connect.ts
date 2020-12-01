ee2631bd75165e65e232363289eaa35bf7980048
Scott Moss <willscottmoss@gmail.com>
2020-12-01 05:49:28 -0800

    chore

--- db/connect.ts
+++ db/connect.ts
@@ -13,20 +13,7 @@ import { Db, MongoClient } from 'mongodb'
 global.mongo = global.mongo || {}
 
 export const connectToDB = async () => {
-  if (!global.mongo.client) {
-    global.mongo.client = new MongoClient(process.env.DATABASE_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      bufferMaxEntries: 0,
-      connectTimeoutMS: 10000,
-    })
-
-    console.log('connecting to DB')
-    await global.mongo.client.connect()
-    console.log('connected to DB')
-  }
-
-  const db: Db = global.mongo.client.db('known')
+  const db = {}
 
   return { db, dbClient: global.mongo.client }
 }
