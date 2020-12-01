f457e96dc03b6a9927827d68c7d6ab9cb2f673ed
Scott Moss <willscottmoss@gmail.com>
2020-12-01 05:57:41 -0800

    chore

--- db/folder.ts
+++ db/folder.ts
@@ -1,22 +1,6 @@
 import { Db } from 'mongodb'
 import { nanoid } from 'nanoid'
 
-export const createFolder = async (db: Db, folder: { createdBy: string; name: string }) => {
-  return db
-    .collection('folders')
-    .insertOne({
-      _id: nanoid(12),
-      ...folder,
-      createdAt: new Date().toDateString(),
-    })
-    .then(({ ops }) => ops[0])
-}
+export const createFolder = async (db: Db, folder: { createdBy: string; name: string }) => {}
 
-export const getFolders = async (db: Db, userId: string) => {
-  return db
-    .collection('folders')
-    .find({
-      createdBy: userId,
-    })
-    .toArray()
-}
+export const getFolders = async (db: Db, userId: string) => {}
--- db/user.ts
+++ db/user.ts
@@ -1,5 +1,3 @@
 import { Db } from 'mongodb'
 
-export const getUserById = async (db: Db, id: string) => {
-  return db.collection('users').findOne({ _id: id })
-}
+export const getUserById = async (db: Db, id: string) => {}
