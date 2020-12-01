963cfaa331922ba90ca276bedc45878550ec856b
Scott Moss <willscottmoss@gmail.com>
2020-12-01 09:57:59 -0800

    chore

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Container from '../components/container'
 import Hero from '../components/hero'
 import HomeNav from '../components/homeNav'
 import FeatureSection from '../components/featureSection'
+import { home } from '../content'
 
 const Home: FC<{ content: { hero: any; features: any[] } }> = ({ content }) => {
   return (
@@ -43,6 +44,14 @@ Home.defaultProps = {
     features: [{ title: 'default feature', body: 'default body' }],
     hero: { title: 'default title', body: 'default body' },
   },
+}
+
+export function getStaticProps() {
+  return {
+    props: {
+      content: home.published,
+    },
+  }
 }
 
 export default Home
